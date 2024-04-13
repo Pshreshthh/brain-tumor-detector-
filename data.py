718bc02fb0dac43d1c9e65c438534db5c60227ee
prakhar shreshthh gupta <157401008+Pshreshthh@users.noreply.github.com>
2024-04-13 16:06:38 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,11 @@
+#importing necessary modules
+import tensorflow as tf
+import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import cv2
+import imutils
+import matplotlib.pyplot as plt
+from os import listdir
+import time    
+
+%matplotlib inline
